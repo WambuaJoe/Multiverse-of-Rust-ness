1cbc6b9713c952735055a758befdca88ea6d6241
WambuaJoe <kaytujohn254@gmail.com>
2024-03-05 17:53:04 +0300

    create hello-world project

---
+++ hello-world/target/debug/deps/hello_world-30ca67dce09a3d95.d
@@ -0,0 +1,5 @@
+/home/wambuajoe/.ssh/projects/Multiverse-of-Rust-ness/hello-world/target/debug/deps/hello_world-30ca67dce09a3d95: src/main.rs
+
+/home/wambuajoe/.ssh/projects/Multiverse-of-Rust-ness/hello-world/target/debug/deps/hello_world-30ca67dce09a3d95.d: src/main.rs
+
+src/main.rs:
---
+++ hello-world/target/debug/hello-world.d
@@ -0,0 +1,1 @@
+/home/wambuajoe/.ssh/projects/Multiverse-of-Rust-ness/hello-world/target/debug/hello-world: /home/wambuajoe/.ssh/projects/Multiverse-of-Rust-ness/hello-world/src/main.rs
